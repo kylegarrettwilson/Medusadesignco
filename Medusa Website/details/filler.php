08dc3c54e3726c4b48964b543de7023f29dc1211
kylegarrettwilson <LetItBe@c-67-201-224-156.reshall.wwu.edu>
2017-01-11 00:02:00 -0800

    work

---
+++ Medusa Website/details/filler.php
@@ -0,0 +1,239 @@
+
+
+<?php
+
+
+
+
+
+  echo "<div id=\"retain\">";
+
+
+
+
+
+
+
+       echo "<article>
+            <p>
+                We are proud to use Bootstrap mobile-ready technology developed by Twitter to make sure each design
+                functions properly with any screen size. Our cutting-edge designs utilize the most current landing
+                page User Experience (UX) and User Interaction (UI) technologies. With the advent of the smartphone,
+                the ability to scroll and view all website content on one page is invaluable to your business and
+                to your customer. Let BRM launch your small business into the realm of artistically professional websites.
+            </p>
+        </article>";
+
+
+
+
+
+
+       echo "<h2>Website Attributes</h2>
+        <p>These are the technical details associated with this website.</p>
+        <div class=\"table-responsive\">
+            <table class=\"table\">
+                <tr>
+                    <th>Bootstrap:</th>
+                    <td>Compatible with 3.3.X</td>
+                </tr>
+                <tr>
+                    <th>Mobile First Design:</th>
+                    <td>Yes</td>
+                </tr>
+                <tr>
+                    <th>Responsive Design:</th>
+                    <td>Yes</td>
+                </tr>
+                <tr>
+                    <th>Fluid Grid:</th>
+                    <td>Yes</td>
+                </tr>
+                <tr>
+                    <th>Browser Compatibility:</th>
+                    <td>IE<br>
+                        Latest Chrome<br>
+                        Latest Firefox<br>
+                        Latest Opera<br>
+                        Latest Safari</td>
+                </tr>
+                <tr>
+                    <th>Released:</th>
+                    <td>09-17-2016</td>
+                </tr>
+                <tr>
+                    <th>Build Time:</th>
+                    <td>2-3 days</td>
+                </tr>
+                <tr>
+                    <th>Uses:</th>
+                    <td>HTML5
+                        CSS3<br>
+                        JavaScript<br>
+                        jQuery<br>
+                        PHP</td>
+                </tr>
+
+            </table>
+        </div>
+    </div>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   echo "<div id=\"second_half\">";
+
+
+       echo "<section>";
+
+
+           echo "<h1>Item Details<a class=\"btn btn-primary btn-lg\" href=\"\">Live
+                    Preview</a></h1>
+
+            <article>
+                <p>
+                    The days of cluttered, multi-paged websites are going in the same direction as parachute pants and floppy
+                    disks: extinct.  There is a massive push by users to experience clean websites that do not overwhelm them
+                    with tacky colors and flashing banners. As the population experiences more and more saturation with advertisements,
+                    it is becoming increasingly important to grab the customer’s attention upon the first click to your website.
+                    The way BRM approaches this changing digital environment is by building large images near the top of the page,
+                    which help to visually display what the business is about at first glance; this aids with both brand
+                    recognition and helps you stand out from competitors. We also utilize techniques that help direct the
+                    users’ sight through the use of smooth animations and photo galleries. It is important to balance the
+                    website visually by mixing use of the content and image to ensure that one does not overshadow the other.
+                    Many of our designs use section headers that are accompanied by visual queues, such as word art, to help
+                    the user quickly find the information they are wanting.  At BRM, we aim to ensure that the content of
+                    your website is displayed in a neat and orderly fashion, making it both appealing and easy to gather
+                    needed information for the customer.
+                    <a href=\"edison/index.html\">HERE</a> is the Roadmap that you will receive a link to after purchase.
+                    Don't have a logo? No problem! Let us design a custom logo for your business! Click
+                    <a class='activate_modal' name='second_window' href='#'>HERE</a> to learn more!
+                </p>
+
+
+            </article>
+
+
+            <div id=\"usability\">
+                <h2>Usability Test Results</h2>
+
+                <article>
+                    <p>
+                        Usability testing is a critical step in the web design process. The main disciplines that BRM aims
+                        to test are learnability and efficiency. Learnability is how easy it is for a new user to accomplish
+                        tasks the first time they visit your site. Efficiency is how quickly users can complete tasks on your
+                        site after they familiarize themselves with its use. The reason these two user interactions (UI) are
+                        so important is because people will go to the next site if the first site they visit is not usable or
+                        cannot be quickly be understood. Your business can have the most beautiful website in the world, but
+                        people will leave immediately if they are unable to figure out how to navigate your site quickly. BRM
+                        focuses on providing designs that are able to adapt the view of the website to the screen size of the
+                        user’s device to aid in the website’s ease of use across devices.
+                    </p>
+                </article><br>
+
+
+                <div class=\"table-responsive\">
+                    <table class=\"table\">
+                        <thead>
+                        <tr>
+                            <th>Country:</th>
+                            <th>End Result:</th>
+                            <th>Error Rate:</th>
+                            <th>Success Rate:</th>
+                            <th>Task Time:</th>
+                            <th>Tests Ran:</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>USA</td>
+                            <td>Website is ready for distribution</td>
+                            <td>0%</td>
+                            <td>100%</td>
+                            <td>8.5s</td>
+                            <td>20</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+
+
+
+
+
+
+        </section>
+
+    </div>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    echo "<!-- Footer Starts -->
+    <div class=\"footer\">
+        <div class=\"overlay spacer text-center\">
+            <div class=\"container\">
+                <h2>We are <b>Medusa Design Co.</b></h2>
+                <p>Artistically Professional Websites For Small Independent Businesses</p><br>
+
+                <p>114 W Magnolia St. Suite #505<br>Bellingham, WA 98225</p>
+                <ul class=\"list-unstyled\">
+                    <li><i class=\"fa fa-phone fa-1x\" style=\"padding-bottom:15px;\"></i><a href=\"tel:541-788-7601\">(541) 788 7601</a></li>
+                    <li><i class=\"fa fa-envelope-o fa-1x\" style=\"padding-bottom:15px;\"></i><a href=\"mailto:medusadesignco@gmail.com\">Email Us</a>
+                    <li><i class=\"fa fa-gavel fa-1x\" style=\"padding-bottom:15px;\"></i><a href=\"\">Legal</a>
+                    </li>
+                </ul>
+                <br>
+
+
+                <div class=\"wowload fadeInUp social\"><a href=\"https://www.facebook.com/bootstraproadmap/\"><i class=\"fa fa-facebook fa-2x\"></i></a> <a href=\"https://www.instagram.com/bootstrap_roadmap/\"><i class=\"fa fa-instagram fa-2x\"></i></a> <a href=\"https://twitter.com/BootstrapRdMap?lang=en\"><i class=\"fa fa-twitter fa-2x\"></i></a> </div><br>
+
+                <div class=\"text-center copyright\">Medusa Design Co. Copyright 2016</div>
+
+            </div>
+        </div>
+    </div>"
+
+
+
+?>
