0dd9fd1e9367f30f1230fb19963fd976bdcf539c
kylegarrettwilson <LetItBe@c-67-201-224-75.reshall.wwu.edu>
2017-01-26 19:31:31 -0800

    work

---
+++ lander-4-one-page-sites-6-coming-soon-pages/Coming-soon-3/notify-me.php
@@ -0,0 +1,33 @@
+<?php
+
+## CONFIG ##
+
+# LIST EMAIL ADDRESS
+$recipient = "info@yourdomain.com";
+
+# SUBJECT (Subscribe/Remove)
+$subject = "Notify Subscription";
+
+## FORM VALUES ##
+
+# SENDER - WE ALSO USE THE RECIPIENT AS SENDER IN THIS SAMPLE
+# DON'T INCLUDE UNFILTERED USER INPUT IN THE MAIL HEADER!
+$sender = $recipient;
+
+# MAIL BODY
+$body .= "Name: ".$_REQUEST['Name']." \n";
+$body .= "Email: ".$_REQUEST['email']." \n";
+# add more fields here if required
+
+## SEND MESSAGE ##
+
+mail( $recipient, $subject, $body, "From: $sender" ) or die ("Mail could not be sent.");
+
+## SHOW RESULT PAGE ##
+
+echo '<script language="javascript">';
+echo 'alert("Your subscription has been registered.");';
+echo 'window.location.href="index.html";';
+echo '</script>';
+
+?>
---
+++ lander-4-one-page-sites-6-coming-soon-pages/README.txt
@@ -0,0 +1,5 @@
+Hello, thank you for your purchase. For detailed documentation, 
+check inside each template's folder (_DOCUMENTATION)
+
+Best regards and don't forget to rate us!
+Dreamcoder
